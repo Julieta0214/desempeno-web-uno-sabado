2e8356eb9df8899842c8e7f8e750552da6ff5d10
JhonTabar <tabaresjhonatan771@gmail.com>
2025-09-27 13:51:13 -0500

    Fase 2: Implementadas funciones para ventas y reposición de stock

--- 102042874-JulianaJimenezMolina/app.js
+++ 102042874-JulianaJimenezMolina/app.js
@@ -8,3 +8,31 @@ function mostrarInventario() {
 }
 
 mostrarInventario();
+
+const registrarVenta = function(cantidadVendida) {
+  if (cantidadVendida <= stockProducto) {
+    stockProducto -= cantidadVendida;
+    ventasTotales += cantidadVendida;
+    console.log(`Venta realizada: ${cantidadVendida} unidades de ${nombreProducto}.`);
+  } else {
+    console.log("Stock insuficiente para esta venta.");
+  }
+};
+
+mostrarInventario();
+
+const reponerStock = (cantidadRepuesta) => {
+  stockProducto += cantidadRepuesta;
+  console.log(`Se añadieron ${cantidadRepuesta} unidades. Nuevo stock: ${stockProducto}.`);
+};
+
+
+registrarVenta(10);   
+registrarVenta(45);  
+reponerStock(20);     
+registrarVenta(5);    
+
+
+mostrarInventario();
+
+
